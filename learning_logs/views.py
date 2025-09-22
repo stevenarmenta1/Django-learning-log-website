f9b5391549045ca06cb58d30de8a0faec2852ed7
Steven Armenta <armentaa.steven@gmail.com>
2025-09-22 16:03:58 -0700

    Added files.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Topic
 
-# Create your views here.
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
