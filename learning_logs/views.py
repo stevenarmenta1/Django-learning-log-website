3e87f308f59abf5288014726c94da6999f61cc4b
Steven Armenta <armentaa.steven@gmail.com>
2025-09-23 15:21:56 -0700

    I added a logout button, I made security improvements so only users can see their own information.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    '''Show all topics.'''
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
+@login_required
 def topic(request, topic_id):
     topic = get_object_or_404(Topic, id=topic_id)
+    # Make sure the topic belongs to the current user. 
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
-
+@login_required
 def new_topic(request):
     '''Add a new topic.'''
     if request.method != 'POST':
@@ -27,7 +37,9 @@ def new_topic(request):
         # POST data submitted; process data. 
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     
     # Display a blank or invalid form. 
@@ -35,6 +47,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     '''Add a new entry for a particular topic.'''
     topic = Topic.objects.get(id=topic_id)
@@ -54,3 +67,25 @@ def new_entry(request, topic_id):
     # Display aa blank or invalid form. 
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+@login_required
+def edit_entry(request, entry_id):
+    '''Edit an existing entry.'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data. 
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
