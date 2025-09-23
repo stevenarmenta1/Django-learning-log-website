7ee6336e6773cf526acbc130f7d5a230de01301c
Steven Armenta <armentaa.steven@gmail.com>
2025-09-23 15:48:19 -0700

    installed django-bootstrap5

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     # My apps. 
     'learning_logs',
     'accounts',
+
+    # Third-party apps.
+    'django_bootstrap5',
+
     # Default Django apps.
     'django.contrib.admin',
     'django.contrib.auth',
